f840a383531fc2c268e6bbec104011e9e7922e61
root <root@betandrelax.com>
2015-03-31 11:02:25 +0200

    addThis

--- sites/default/themes/arras/page.tpl.php
+++ sites/default/themes/arras/page.tpl.php
@@ -109,11 +109,12 @@ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = 
 -->
 <!-- {/literal} END JIVOSITE CODE -->  </div></div> <!-- /#page, /#page-wrapper -->
 
- <!-- BEGIN JIVOSITE CODE {literal} -->
-<!--
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55119d9126f25dac" async="async"></script>
+
+
+<!-- BEGIN JIVOSITE CODE {literal} -->
 <script type='text/javascript'>
 (function(){ var widget_id = '1KZFDqM8Yx';
 var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
--->
 <!-- {/literal} END JIVOSITE CODE -->
-  </div></div> <!-- /#page, /#page-wrapper -->
