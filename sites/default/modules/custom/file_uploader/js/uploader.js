92822f301118126692e50164f15469f003070b29
root <root@debian>
2015-11-19 11:50:35 +0100

    deleted

--- sites/default/modules/custom/file_uploader/js/uploader.js
+++
@@ -1,9 +0,0 @@
- function WriteToFile() {
-    
-    set fso = CreateObject("Scripting.FileSystemObject");  
-    set s = fso.CreateTextFile("C:\test.txt", True);
-    s.writeline("HI");
-    s.writeline("Bye");
-    s.writeline("-----------------------------");
-    s.Close();
- }
